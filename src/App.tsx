469111131f2a670e189404226aabf2b074bdf46d
Adam Szokalski <szokalskiadam@gmail.com>
2022-12-27 23:59:08 +0100

    BLE module initialized on IOS and Android

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {RootStack} from './Components/RootStack';
 import {HomeScreen} from './Components/HomeScreen';
 import {DetailsScreen} from './Components/DetailsScreen';
+import BleManager from 'react-native-ble-manager';
 
 export default function App() {
+  useEffect(() => {
+    BleManager.start({showAlert: false}).then(() => {
+      // Success code
+      console.log('Module initialized');
+    });
+  }, []);
   return (
     <NavigationContainer>
       <RootStack.Navigator>
