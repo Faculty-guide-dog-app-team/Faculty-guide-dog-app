1bcb49809a2a1adf4e1bf2dc275cb71797766e5c
Adam Szokalski <szokalskiadam@gmail.com>
2023-01-02 00:38:12 +0100

    Łączenie się z sensorami

--- src/Tools/Sensor.ts
+++ src/Tools/Sensor.ts
@@ -1,15 +1,79 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import {AdvertisingData, Peripheral} from 'react-native-ble-manager';
+import BleManager, {
+  AdvertisingData,
+  Peripheral,
+} from 'react-native-ble-manager';
+
+const SENSOR_UPDATE_INTERVAL_MS = 1000;
+export const RSSI_THRESHOLD = -70;
+const LOCTION_SHATING_SERVICE = '6951f9c0-2375-49f5-8da9-f45c9f067dcb';
+const X_CHARACTERISTIC_UUID = '8eeec66e-71ce-11ed-a1eb-0242ac120002';
+const Y_CHARACTERISTIC_UUID = '74e7237c-71d0-11ed-a1eb-0242ac120002';
 
 export class Sensor implements Peripheral {
   rssi: number;
   id: string;
   name?: string | undefined;
   advertising: AdvertisingData;
+  updateHandle: number | null;
+  x: number | null;
+  y: number | null;
   constructor(peripheral: Peripheral) {
     this.id = peripheral.id;
     this.rssi = peripheral.rssi;
     this.name = peripheral.name;
     this.advertising = peripheral.advertising;
+    this.updateHandle = null;
+    this.x = null;
+    this.y = null;
+  }
+
+  private update() {
+    BleManager.readRSSI(this.id)
+      .then((value: unknown) => {
+        this.rssi = value as number;
+        console.log(this.rssi);
+        if (this.rssi < RSSI_THRESHOLD) {
+          this.disconnect();
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    BleManager.retrieveServices(this.id).then(() => {
+      BleManager.read(this.id, LOCTION_SHATING_SERVICE, Y_CHARACTERISTIC_UUID)
+        .then((value: unknown) => {
+          this.y = value as number;
+          console.log(this.y);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      BleManager.read(this.id, LOCTION_SHATING_SERVICE, X_CHARACTERISTIC_UUID)
+        .then((value: unknown) => {
+          this.x = value as number;
+          console.log(this.x);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    });
+  }
+
+  public startListening() {
+    this.updateHandle = setInterval(
+      this.update.bind(this),
+      SENSOR_UPDATE_INTERVAL_MS,
+    );
+  }
+
+  public disconnect() {
+    BleManager.disconnect(this.id);
+  }
+
+  public destroy() {
+    if (this.updateHandle !== null) {
+      clearInterval(this.updateHandle);
+    }
   }
 }
--- src/Tools/useBLE.ts
+++ src/Tools/useBLE.ts
@@ -2,41 +2,24 @@
 import React, {useEffect, useState} from 'react';
 import {enableBLEAndroid} from './BLE';
 import BleManager, {Peripheral} from 'react-native-ble-manager';
-import {Sensor} from './Sensor';
+import {RSSI_THRESHOLD, Sensor} from './Sensor';
 import {NativeEventEmitter, NativeModules} from 'react-native';
 
 const bleManagerEmitter = new NativeEventEmitter(NativeModules.BleManager);
+interface DisconnectData {
+  peripheral: string;
+  code?: number;
+  status?: number;
+  domain?: string;
+}
 
 export const useBLE = () => {
+  const [moduleInitialized, setModuleInitialized] = useState(false);
+  const [isScanning, setIsScanning] = useState(false);
   const [sensorsConnected, setSensorsConnected] = useState<Array<Sensor>>([]);
   const [pendingConnections, setPendingConnections] = useState<Array<Sensor>>(
     [],
   );
-
-  function handleDiscovery(peripheral: Peripheral) {
-    console.log(peripheral);
-    if (pendingConnections.length + sensorsConnected.length >= 3) {
-      return;
-    }
-
-    let newSensor = new Sensor(peripheral);
-    BleManager.connect(peripheral.id).then(() => {
-      setSensorsConnected(curret => [...curret, newSensor]);
-      setPendingConnections(current =>
-        current.filter(sensor => sensor !== newSensor),
-      );
-    });
-
-    setPendingConnections(current => [...current, newSensor]);
-  }
-
-  bleManagerEmitter.addListener('BleManagerDiscoverPeripheral', peripheral => {
-    handleDiscovery(peripheral);
-  });
-
-  bleManagerEmitter.addListener('BleManagerStopScan', () => {
-    console.log('Scan stopped');
-  });
 
   useEffect(() => {
     if (!enableBLEAndroid()) {
@@ -44,14 +27,96 @@ export const useBLE = () => {
       return;
     }
 
-    BleManager.start({showAlert: false}).then(() => {
-      console.log('Module initialized');
+    function handleDiscovery(peripheral: Peripheral) {
+      console.log('Sensor discovered', peripheral);
+      if (pendingConnections.length + sensorsConnected.length >= 3) {
+        return;
+      }
+      if (peripheral.rssi > RSSI_THRESHOLD) {
+        return;
+      }
 
-      BleManager.scan(['6951f9c0-2375-49f5-8da9-f45c9f067dcb'], 5).then(() => {
-        console.log('Scan started');
+      let newSensor = new Sensor(peripheral);
+      BleManager.connect(peripheral.id)
+        .then(() => {
+          console.log('Sensor connected', peripheral.id);
+          setPendingConnections(current =>
+            current.filter(sensor => sensor.id !== newSensor.id),
+          );
+          newSensor.startListening();
+          setSensorsConnected(curret => [...curret, newSensor]);
+        })
+        .catch(error => {
+          // Failure code
+          console.error(error);
+        });
+
+      setPendingConnections(current => [...current, newSensor]);
+    }
+
+    function handleDisconnection(peripheralID: string) {
+      console.log('Sensor disconnected', peripheralID);
+      setSensorsConnected(current => {
+        let newSensorsConnected = [];
+        for (let sensor of current) {
+          if (sensor.id !== peripheralID) {
+            newSensorsConnected.push(sensor);
+          } else {
+            sensor.destroy();
+            console.log('destroy');
+          }
+        }
+        return newSensorsConnected;
       });
-    });
-  }, []);
+    }
+
+    if (!moduleInitialized) {
+      setModuleInitialized(true);
+      bleManagerEmitter.addListener(
+        'BleManagerDiscoverPeripheral',
+        peripheral => {
+          handleDiscovery(peripheral);
+        },
+      );
+
+      bleManagerEmitter.addListener(
+        'BleManagerDisconnectPeripheral',
+        (data: DisconnectData) => {
+          handleDisconnection(data.peripheral);
+        },
+      );
+
+      bleManagerEmitter.addListener('BleManagerStopScan', () => {
+        setIsScanning(false);
+        console.log('Scan stopped');
+      });
+      BleManager.start({showAlert: false})
+        .then(() => {
+          console.log('Module initialized');
+        })
+        .catch(error => {
+          setModuleInitialized(false);
+          console.error(error);
+        });
+    }
+  }, [moduleInitialized, pendingConnections.length, sensorsConnected]);
+
+  useEffect(() => {
+    if (moduleInitialized && !isScanning && sensorsConnected.length < 3) {
+      setIsScanning(true);
+      BleManager.scan(['6951f9c0-2375-49f5-8da9-f45c9f067dcb'], 5, false)
+        .then(() => {
+          console.log('Scan started');
+        })
+        .catch(error => {
+          console.error('Scan falied', error);
+          setIsScanning(false);
+        });
+    }
+    if (isScanning && sensorsConnected.length >= 3) {
+      BleManager.stopScan();
+    }
+  }, [moduleInitialized, isScanning, sensorsConnected]);
 
   return sensorsConnected;
 };
